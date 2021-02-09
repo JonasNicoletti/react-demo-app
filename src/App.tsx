549cc7d9099089356d35f28bb998e700111761c8
Jonas Nicoletti <sjnao89@gmail.com>
2021-02-09 09:54:15 +0100

    add common module

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Footer from "modules/common/components/footer/Footer";
+import Header from "modules/common/components/header/Header";
+import Home from "modules/home/components/Home";
 import React from "react";
 import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+      <Home />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
